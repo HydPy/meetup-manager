9b17317cf8de7e6afe703a12e4eb7526c9f51cda
GoelJatin <jatingoel1037@hotmail.com>
2018-10-14 03:18:48 +0530

    Add Models

--- HydPy/main/admin.py
+++ HydPy/main/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import (
+    User,
+    Speaker,
+    Venue,
+    RegisteredUser
+)
 
 admin.site.register(User)
+admin.site.register(Speaker)
+admin.site.register(Venue)
+admin.site.register(RegisteredUser)
--- HydPy/main/models.py
+++ HydPy/main/models.py
@@ -1,8 +1,43 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
 class User(models.Model):
     username = models.CharField(max_length=32)
-    FirstName = models.CharField(max_length=50)
-    LastName = models.CharField(max_length=50)
+    FirstName = models.CharField(max_length=50, verbose_name='First Name')
+    LastName = models.CharField(max_length=50, verbose_name='Last Name')
     email = models.CharField(max_length=100)
+    mobile_no_validator = RegexValidator(regex=r'\d{10}', message="Phone number must be 10 digits only!!")
+    mobile = models.CharField(validators=[mobile_no_validator], max_length=10)
+
+    def __str__(self):
+        return f'User: [{self.FirstName} {self.LastName}] with Username: [{self.username}]'
+
+
+class Speaker(User):
+    EmergencyContactName = models.CharField(max_length=100, verbose_name='Emergency Contact')
+    EmergencyContactNumber = models.CharField(validators=[User.mobile_no_validator], max_length=10, verbose_name='Emergency Contact Number')
+    GitHub = models.CharField(max_length=200)
+    Twitter = models.CharField(max_length=200, blank=True)
+    LinkedIn = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f'Speaker: [{self.FirstName} {self.LastName}] with Username: [{self.username}]'
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=1000)
+    description = models.TextField()
+    address = models.TextField()
+    website = models.CharField(max_length=300)
+    contact = models.CharField(max_length=100)
+    verified = models.BooleanField()
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+
+    def __str__(self):
+        return f'Venue: [{self.name}] with website: [{self.website}]'
+
+
+class RegisteredUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
