a0b7b549178afd1363c003a77170d95c05deae91
Jatin Goel <jatingoel1037@hotmail.com>
2020-08-30 13:28:06 +0530

    Run 4 workers and write to stderr

--- src/startup.sh
+++ src/startup.sh
@@ -2,4 +2,4 @@ cd meetup_manager/
 
 pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
-pipenv run gunicorn meetup_manager.wsgi:application --bind 0.0.0.0:8080
+pipenv run gunicorn meetup_manager.wsgi:application --bind 0.0.0.0:8080 -w 4 --log-file -
