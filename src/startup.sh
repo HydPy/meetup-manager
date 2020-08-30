1b437743137b08ae76b05f994b259ff6cde49076
Jatin Goel <jatingoel1037@hotmail.com>
2020-08-30 14:06:07 +0530

    Collect static files and serve from nginx server

--- src/startup.sh
+++ src/startup.sh
@@ -1,5 +1,7 @@
 cd meetup_manager/
 
 pipenv run python manage.py makemigrations
-pipenv run python manage.py migrate
+pipenv run python manage.py migrate --no-input
+pipenv run python manage.py collectstatic --no-input --clear
+
 pipenv run gunicorn meetup_manager.wsgi:application --bind 0.0.0.0:8080 -w 4 --log-file -
